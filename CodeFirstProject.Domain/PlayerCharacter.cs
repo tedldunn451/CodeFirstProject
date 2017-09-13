fa37f15a7495ef73f998651dcfafa274c53d0832
Ted Dunn <tldunn451@gmail.com>
2017-09-13 12:02:37 -0700

    Add Domain project and PlayerCharacter class

---
+++ CodeFirstProject.Domain/PlayerCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstProject.Domain
+{
+    public class PlayerCharacter
+    {
+		public string CharacterName { get; set; }
+		public int CharacterLevel { get; set; }
+		public int ExperiencePoints { get; set; }
+		public int HitPoints { get; set; }
+
+		enum Race { Human, Dwarf, Elf, Halfling};
+		enum CharacterClass { Fighter, Wizard, Thief, Cleric};
+	}
+}
